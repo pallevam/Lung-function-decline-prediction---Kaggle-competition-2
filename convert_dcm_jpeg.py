626d86d5db139b3a8a6cc7a6269542fbe87ca680
Vamsi <pvamsi170@gmail.com>
2020-08-17 12:32:18 +0530

    first commit

---
+++ convert_dcm_jpeg.py
@@ -0,0 +1,30 @@
+import pydicom as dicom
+import os, cv2, glob
+
+
+# method not working
+def dcm_to_jpg(filepath, jpgpath):
+    PNG = False
+    images_path = os.listdir(filepath)
+    for n, image in enumerate(images_path):
+        ds = dicom.dcmread(os.path.join(filepath, image))
+        pixel_array_numpy = ds.pixel_array
+        if PNG == False:
+            image = image.replace('.dcm', '.jpg')
+        else:
+            image = image.replace('.dcm', '.png')
+        cv2.imwrite(os.path.join(jpgpath, image), pixel_array_numpy)
+        if n % 50 == 0:
+            print('{} image converted'.format(n))
+
+
+# working but have to convert again from .png to .jpg
+
+def dcm_to_png(inputdir, outputdir):
+    test_list = [os.path.basename(x) for x in glob.glob(inputdir + '*.dcm')]
+
+    for f in test_list:
+        ds = dicom.read_file(inputdir + f) # read dcom image
+        img = ds.pixel_array # get pixel array
+        cv2.imwrite(outputdir + f.replace('.dcm', '.png'), img) # writing png image
+
---
+++ stage_test.py
@@ -0,0 +1,2 @@
+from convert_dcm_jpeg import dcm_to_jpg
+
