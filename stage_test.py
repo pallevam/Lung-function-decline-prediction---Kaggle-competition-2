db5884c56223c16aec42d2320a1371a5d06d6232
Vamsi <pvamsi170@gmail.com>
2020-08-17 12:36:23 +0530

    initial commit 2

--- stage_test.py
+++ stage_test.py
@@ -1,2 +1,11 @@
-from convert_dcm_jpeg import dcm_to_jpg
+from convert_dcm_jpeg import dcm_to_jpg, dcm_to_png
 
+# test method
+
+# provide input path where .dcm files are present and output path where .png or .jpg files are to be created
+
+input_path = "path/files.dcm"
+
+output_path = "path/files.jpg"
+
+dcm_to_png(input_path, output_path)
